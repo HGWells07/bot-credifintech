1d0c0c313f3fa4056305cbe7e9ca9b26e30a662f
hgwells07 <carlitose07@gmail.com>
2018-08-21 18:28:29 -0500

    Versión estable del bot sin notas

--- index.php
+++ index.php
@@ -45,35 +45,19 @@ $config = [
   
   'facebook'=> [
     //Botencio McBot
-    'token' => Constantes::generateTokenArray()["T_BOTENCIO"],
+    'token' => Constantes::T_BOTENCIO,
     'app_secret' => Constantes::APP_SECRET, 
     'verification'=> Constantes::VERIFICATION,
   ],
-  /*
-  'facebook'=> [
-    //Botencio McBot
-    'token' => Constantes::generateTokenArray()["T_CF"],
-    'app_secret' => Constantes::APP_SECRET, 
-    'verification'=> Constantes::VERIFICATION,
-  ],
-  */
-  /*
-  'facebook'=> [
-    //Credifintech
-    'token' => 'EAAGrT16HtJgBANcy1trAD3kht0pIoW18gHaaUY9DcXjsTGBfifvKxXEhtGox1yd6iWqRlpiAKrTxwmM9Ow1I71x7ZBI0OOFgsxuXD3rx1bxk55NlovIwJAoi5EWpNGYsMRDcKurUZCL2EWxen8fWZCCX9L6c7S2eHiFt0ZC8eZA4qVHym8yOT',
-    'app_secret' => 'e4647b87a6b18da6803bddc3b3349674', 
-    'verification'=>'d8wkg9wkflaaeha54qyhf5yadfjaibs3iwro203852',
-  ]
-  */
 ];
 
 $doctrineCacheDriver = new FilesystemCache(__DIR__);
 $botman = BotManFactory::create($config, new DoctrineCache($doctrineCacheDriver));
 
 $botman->hears('^(?!.*\basesor|ASESOR|Asesor\b).*$', function (BotMan $bot) {
-  //$nombre = $bot->getUser()->getUsername();
-  $nombre = "";
-  $bot -> reply("Bienvenido$nombre. Somos una empresa que se dedica a dar créditos a los sectores de gobierno, IMSS y de la SEP");
+  $nombre = $bot->getUserWithFields(["first_name"]);
+  $nombre = $nombre->getFirstName();
+  $bot -> reply("Bienvenido $nombre. Somos una empresa que se dedica a dar créditos a los sectores de gobierno, IMSS y de la SEP");
   $bot -> startConversation(new MenuConversation($nombre));
 });
 
